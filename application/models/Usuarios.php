775f6a0d9ecb084050494f699a0e84ad8d1a42ff
ruben <ruben@192.168.10.92>
2018-01-19 00:19:14 -0300

    Version1 proyecto

---
+++ application/models/Usuarios.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Usuarios
+ *
+ * @author ruben
+ */
+class Usuarios {
+    //put your code here
+}
