082a5ffc556b230107955e7e04ba8fa1c6fcb934
jimrooto <d.morenor@outlook.com>
2018-01-20 18:07:16 -0300

    url

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -88,8 +88,7 @@ $autoload['drivers'] = array();
 | Prototype:
 |
 |	$autoload['helper'] = array('url', 'file');
-*/
-$autoload['helper'] = array();
+*/$autoload['helper'] = array('url');
 
 /*
 | -------------------------------------------------------------------
